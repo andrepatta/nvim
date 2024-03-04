207afa0f103298f403cd78e3db574d601ab31cd4
andrepatta <andre.patta@electroneum.com>
2024-03-03 21:24:44 -0300

    neotree git icons

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,6 +1,19 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        -- hide_dotfiles = false,
+        -- hide_gitignored = true,
+        -- hide_by_name = {
+        --   ".github",
+        --   ".gitignore",
+        --   "package-lock.json",
+        -- },
+        never_show = { ".git" },
+      },
+    },
     window = {
       mappings = {
         ["h"] = "close_node",
@@ -23,12 +36,12 @@ return {
           -- Change type
           added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
           modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-          deleted = "✖", -- this can only be used in the git_status source
-          renamed = "󰁕", -- this can only be used in the git_status source
+          deleted = "", -- this can only be used in the git_status source
+          renamed = "➜", -- this can only be used in the git_status source
           -- Status type
-          untracked = "",
+          untracked = "◌",
           ignored = "",
-          unstaged = "󰄱",
+          unstaged = "",
           staged = "",
           conflict = "",
         },
